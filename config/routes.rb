759199c6a700689b817508b7db6b65981314ca2d
tasuke031 <yosuket031@gmail.com>
2020-09-25 21:49:06 +0900

    messagesコントローラーの作成
    ルーティングの設定(ネスト)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'rooms#index'
-  resources 'rooms', only: [:index, :create, :show, :destroy]
+  resources 'rooms', only: [:index, :create, :show, :destroy] do
+    resources 'messages', only: :create
+  end
 end
